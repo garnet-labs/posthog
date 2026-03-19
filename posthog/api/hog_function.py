c58171bed420387601b607f50e0e6aced752d061
Ross <rossagray@gmail.com>
2026-03-19 16:15:18 +0000

    fix: change batch export interval from day to hour

--- posthog/api/hog_function.py
+++ posthog/api/hog_function.py
@@ -713,7 +713,7 @@ class HogFunctionViewSet(
         batch_export_data = {
             "name": hog_function.name,
             "paused": True,
-            "interval": "day",
+            "interval": "hour",
             "model": "events",
             "filters": hog_function.filters.get("events", []) if hog_function.filters else [],
             "destination": {
