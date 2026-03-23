b25ae69a14615129df6e83efb23a8572244ae0b5
Sam Pennington <sam@posthog.com>
2026-03-23 19:23:09 +0000

    Adds stories for insights

--- frontend/src/scenes/insights/Retention.stories.tsx
+++ frontend/src/scenes/insights/Retention.stories.tsx
@@ -1,11 +1,17 @@
 import { samplePersonProperties, sampleRetentionPeopleResponse } from 'scenes/insights/__mocks__/insight.mocks'
 
-import { Meta, StoryObj } from '@storybook/react'
+import { Meta, StoryFn, StoryObj } from '@storybook/react'
+import { router } from 'kea-router'
 
+import { useOnMountEffect } from 'lib/hooks/useOnMountEffect'
 import { App } from 'scenes/App'
 import { createInsightStory } from 'scenes/insights/__mocks__/createInsightScene'
 
-import { mswDecorator } from '~/mocks/browser'
+import { mswDecorator, useStorybookMocks } from '~/mocks/browser'
+
+import retentionInsightFixture from '../../mocks/fixtures/api/projects/team_id/insights/retention.json'
+
+const retentionInsight = retentionInsightFixture as any
 
 type Story = StoryObj<typeof App>
 const meta: Meta = {
@@ -63,6 +69,104 @@ RetentionEditViewports.parameters = {
     testOptions: {
         waitForSelector: '[data-attr=trend-line-graph] > canvas',
         viewportWidths: ['medium', 'wide', 'superwide'],
+    },
+}
+
+// Error, Empty, Loading states
+
+export const RetentionEmpty: StoryFn = () => {
+    useStorybookMocks({
+        get: {
+            '/api/environments/:team_id/insights/': (_, __, ctx) => [
+                ctx.delay(100),
+                ctx.status(200),
+                ctx.json({ count: 1, results: [{ ...retentionInsight, result: [] }] }),
+            ],
+            '/api/environments/:team_id/persons/retention': sampleRetentionPeopleResponse,
+            '/api/environments/:team_id/persons/properties': samplePersonProperties,
+        },
+        post: {
+            '/api/environments/:team_id/query/': (_, __, ctx) => [
+                ctx.status(200),
+                ctx.json({
+                    results: [],
+                    is_cached: true,
+                }),
+            ],
+        },
+    })
+
+    useOnMountEffect(() => {
+        router.actions.push(`/insights/${retentionInsight.short_id}`)
+    })
+
+    return <App />
+}
+RetentionEmpty.parameters = {
+    testOptions: {
+        waitForSelector: '[data-attr="insight-empty-state"]',
+    },
+}
+
+export const RetentionError: StoryFn = () => {
+    useStorybookMocks({
+        get: {
+            '/api/environments/:team_id/insights/': (_, __, ctx) => [
+                ctx.delay(100),
+                ctx.status(200),
+                ctx.json({ count: 1, results: [{ ...retentionInsight, result: null }] }),
+            ],
+            '/api/environments/:team_id/persons/retention': sampleRetentionPeopleResponse,
+            '/api/environments/:team_id/persons/properties': samplePersonProperties,
+        },
+        post: {
+            '/api/environments/:team_id/query/': (_, __, ctx) => [
+                ctx.delay(100),
+                ctx.status(500),
+                ctx.json({
+                    type: 'server_error',
+                    detail: 'There is nothing you can do to stop the impending catastrophe.',
+                }),
+            ],
+        },
+    })
+
+    useOnMountEffect(() => {
+        router.actions.push(`/insights/${retentionInsight.short_id}`)
+    })
+
+    return <App />
+}
+RetentionError.parameters = {
+    testOptions: {
+        waitForSelector: '[data-attr="insight-retry-button"]',
+    },
+}
+
+export const RetentionLoading: StoryFn = () => {
+    useStorybookMocks({
+        get: {
+            '/api/environments/:team_id/insights/': (_, __, ctx) => [
+                ctx.status(200),
+                ctx.json({ count: 1, results: [{ ...retentionInsight, result: null }] }),
+            ],
+            '/api/environments/:team_id/persons/retention': sampleRetentionPeopleResponse,
+            '/api/environments/:team_id/persons/properties': samplePersonProperties,
+        },
+        post: {
+            '/api/environments/:team_id/query/': (_, __, ctx) => [ctx.delay('infinite')],
+        },
+    })
+
+    useOnMountEffect(() => {
+        router.actions.push(`/insights/${retentionInsight.short_id}`)
+    })
+
+    return <App />
+}
+RetentionLoading.parameters = {
+    testOptions: {
+        waitForLoadersToDisappear: false,
     },
 }
 
--- frontend/src/scenes/insights/views/WorldMap/WorldMap.stories.tsx
+++ frontend/src/scenes/insights/views/WorldMap/WorldMap.stories.tsx
@@ -1,11 +1,15 @@
 import { samplePersonProperties, sampleRetentionPeopleResponse } from 'scenes/insights/__mocks__/insight.mocks'
 
-import { Meta, StoryObj } from '@storybook/react'
+import { Meta, StoryFn, StoryObj } from '@storybook/react'
+import { router } from 'kea-router'
 
+import { useOnMountEffect } from 'lib/hooks/useOnMountEffect'
 import { App } from 'scenes/App'
 import { createInsightStory } from 'scenes/insights/__mocks__/createInsightScene'
 
-import { mswDecorator } from '~/mocks/browser'
+import { mswDecorator, useStorybookMocks } from '~/mocks/browser'
+
+import worldMapInsight from '../../../../mocks/fixtures/api/projects/team_id/insights/trendsWorldMap.json'
 
 type Story = StoryObj<typeof App>
 const meta: Meta = {
@@ -41,3 +45,93 @@ export const Default: Story = createInsightStory(
     require('../../../../mocks/fixtures/api/projects/team_id/insights/trendsWorldMap.json')
 )
 Default.parameters = { testOptions: { waitForSelector: '.WorldMap' } }
+
+export const WorldMapEmpty: StoryFn = () => {
+    useStorybookMocks({
+        get: {
+            '/api/environments/:team_id/insights/': (_, __, ctx) => [
+                ctx.delay(100),
+                ctx.status(200),
+                ctx.json({ count: 1, results: [{ ...worldMapInsight, result: [] }] }),
+            ],
+        },
+        post: {
+            '/api/environments/:team_id/query/': (_, __, ctx) => [
+                ctx.status(200),
+                ctx.json({
+                    results: [],
+                    is_cached: true,
+                }),
+            ],
+        },
+    })
+
+    useOnMountEffect(() => {
+        router.actions.push(`/insights/${worldMapInsight.short_id}`)
+    })
+
+    return <App />
+}
+WorldMapEmpty.parameters = {
+    testOptions: {
+        waitForSelector: '[data-attr="insight-empty-state"]',
+    },
+}
+
+export const WorldMapError: StoryFn = () => {
+    useStorybookMocks({
+        get: {
+            '/api/environments/:team_id/insights/': (_, __, ctx) => [
+                ctx.delay(100),
+                ctx.status(200),
+                ctx.json({ count: 1, results: [{ ...worldMapInsight, result: null }] }),
+            ],
+        },
+        post: {
+            '/api/environments/:team_id/query/': (_, __, ctx) => [
+                ctx.delay(100),
+                ctx.status(500),
+                ctx.json({
+                    type: 'server_error',
+                    detail: 'There is nothing you can do to stop the impending catastrophe.',
+                }),
+            ],
+        },
+    })
+
+    useOnMountEffect(() => {
+        router.actions.push(`/insights/${worldMapInsight.short_id}`)
+    })
+
+    return <App />
+}
+WorldMapError.parameters = {
+    testOptions: {
+        waitForSelector: '[data-attr="insight-retry-button"]',
+    },
+}
+
+export const WorldMapLoading: StoryFn = () => {
+    useStorybookMocks({
+        get: {
+            '/api/environments/:team_id/insights/': (_, __, ctx) => [
+                ctx.status(200),
+                ctx.json({ count: 1, results: [{ ...worldMapInsight, result: null }] }),
+            ],
+        },
+        post: {
+            '/api/environments/:team_id/query/': (_, __, ctx) => [ctx.delay('infinite')],
+        },
+    })
+
+    useOnMountEffect(() => {
+        router.actions.push(`/insights/${worldMapInsight.short_id}`)
+    })
+
+    return <App />
+}
+WorldMapLoading.parameters = {
+    testOptions: {
+        waitForLoadersToDisappear: false,
+    },
+}
