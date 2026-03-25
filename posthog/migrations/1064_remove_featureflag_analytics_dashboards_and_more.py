e17f81e5e7718d083d8c2dfd50fe1a13981c7fb4
Julian Bez <julian@posthog.com>
2026-03-25 11:15:17 +0100

    fix(feature_flags): use db_alias in content type migration for multi-DB safety

--- posthog/migrations/1064_remove_featureflag_analytics_dashboards_and_more.py
+++ posthog/migrations/1064_remove_featureflag_analytics_dashboards_and_more.py
@@ -28,10 +28,10 @@ def _move_content_types(apps, schema_editor, from_label, to_label):
 
         if source and target:
             Permission.objects.using(db_alias).filter(content_type=source).update(content_type=target)
-            source.delete()
+            source.delete(using=db_alias)
         elif source:
             source.app_label = to_label
-            source.save(update_fields=["app_label"])
+            source.save(using=db_alias)
 
 
 def update_content_types(apps, schema_editor):
