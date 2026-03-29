9c8636500ffef6122de4137fc67e5a6160d320d3
Adam Leith <adamleithp@users.noreply.github.com>
2026-03-29 23:20:09 +0100

    update index.ts so all primitives are consumable

--- packages/quill/packages/primitives/src/index.ts
+++ packages/quill/packages/primitives/src/index.ts
@@ -1,8 +1,58 @@
 export { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from './accordion'
-export { Button, buttonVariants } from './button'
 export { Badge, badgeVariants } from './badge'
+export { Button, buttonVariants } from './button'
+export { ButtonGroup, ButtonGroupSeparator, ButtonGroupText, buttonGroupVariants } from './button-group'
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './card'
+export { CardGroup } from './card-group'
 export { Checkbox } from './checkbox'
+export { Chip, ChipClose, ChipGroup } from './chip'
+export { Collapsible, CollapsibleTrigger, CollapsibleContent } from './collapsible'
+export {
+    Combobox,
+    ComboboxInput,
+    ComboboxContent,
+    ComboboxList,
+    ComboboxItem,
+    ComboboxGroup,
+    ComboboxLabel,
+    ComboboxCollection,
+    ComboboxEmpty,
+    ComboboxSeparator,
+    ComboboxChips,
+    ComboboxChip,
+    ComboboxChipsInput,
+    ComboboxTrigger,
+    ComboboxValue,
+    useComboboxAnchor,
+} from './combobox'
+export {
+    Command,
+    CommandDialog,
+    CommandInput,
+    CommandList,
+    CommandEmpty,
+    CommandGroup,
+    CommandItem,
+    CommandShortcut,
+    CommandSeparator,
+} from './command'
+export {
+    ContextMenu,
+    ContextMenuTrigger,
+    ContextMenuContent,
+    ContextMenuItem,
+    ContextMenuCheckboxItem,
+    ContextMenuRadioItem,
+    ContextMenuLabel,
+    ContextMenuSeparator,
+    ContextMenuShortcut,
+    ContextMenuGroup,
+    ContextMenuPortal,
+    ContextMenuSub,
+    ContextMenuSubContent,
+    ContextMenuSubTrigger,
+    ContextMenuRadioGroup,
+} from './context-menu'
 export {
     Dialog,
     DialogClose,
@@ -15,11 +65,77 @@ export {
     DialogTitle,
     DialogTrigger,
 } from './dialog'
+export { DirectionProvider, useDirection } from './direction'
+export {
+    Drawer,
+    DrawerPortal,
+    DrawerOverlay,
+    DrawerTrigger,
+    DrawerClose,
+    DrawerContent,
+    DrawerHeader,
+    DrawerFooter,
+    DrawerTitle,
+    DrawerDescription,
+} from './drawer'
+export {
+    DropdownMenu,
+    DropdownMenuPortal,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuLabel,
+    DropdownMenuItem,
+    DropdownMenuCheckboxItem,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuSub,
+    DropdownMenuSubTrigger,
+    DropdownMenuSubContent,
+} from './dropdown-menu'
+export { Empty, EmptyHeader, EmptyTitle, EmptyDescription, EmptyContent, EmptyMedia } from './empty'
+export {
+    Field,
+    FieldLabel,
+    FieldDescription,
+    FieldError,
+    FieldGroup,
+    FieldLegend,
+    FieldSeparator,
+    FieldSet,
+    FieldContent,
+    FieldTitle,
+} from './field'
 export { Input } from './input'
+export { InputGroup, InputGroupAddon, InputGroupButton, InputGroupText, InputGroupInput, InputGroupTextarea } from './input-group'
+export { Item, ItemCheckbox, ItemMenuItem, ItemMedia } from './item'
+export { Kbd, KbdGroup, KbdText } from './kbd'
 export { Label } from './label'
+export {
+    Menubar,
+    MenubarPortal,
+    MenubarMenu,
+    MenubarTrigger,
+    MenubarContent,
+    MenubarGroup,
+    MenubarSeparator,
+    MenubarLabel,
+    MenubarItem,
+    MenubarShortcut,
+    MenubarCheckboxItem,
+    MenubarRadioGroup,
+    MenubarRadioItem,
+    MenubarSub,
+    MenubarSubTrigger,
+    MenubarSubContent,
+} from './menubar'
 export { Popover, PopoverContent, PopoverTrigger } from './popover'
 export { Progress } from './progress'
 export { RadioGroup, RadioGroupItem } from './radio-group'
+export { ResizableHandle, ResizablePanel, ResizablePanelGroup } from './resizable'
+export { ScrollArea, ScrollBar } from './scroll-area'
 export {
     Select,
     SelectContent,
@@ -32,13 +148,15 @@ export {
 } from './select'
 export { Separator } from './separator'
 export { Skeleton } from './skeleton'
+export { SkeletonText } from './skeleton-text'
 export { Slider } from './slider'
+export { Toaster } from './sonner'
+export { Spinner } from './spinner'
 export { Switch } from './switch'
 export { Tabs, TabsContent, TabsList, TabsTrigger } from './tabs'
 export { Textarea } from './textarea'
 export { Toggle, toggleVariants } from './toggle'
 export { ToggleGroup, ToggleGroupItem } from './toggle-group'
 export { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './tooltip'
-export { Spinner } from './spinner'
 export { ThemeProvider, useTheme, type Theme } from './theme-provider'
 export { cn } from './lib/utils'
