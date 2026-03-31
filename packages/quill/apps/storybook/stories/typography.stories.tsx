f34354398cf18295fa7632afa24f2af1ff59cbe5
Adam Leith <adamleithp@users.noreply.github.com>
2026-03-31 23:10:20 +0100

    update typography, remove visual images

--- packages/quill/apps/storybook/stories/typography.stories.tsx
+++ packages/quill/apps/storybook/stories/typography.stories.tsx
@@ -12,14 +12,13 @@ export const Default: Story = {
     render: () => {
         return (
             <div className="space-y-6">
-                <h1 className="text-2xl font-bold">Heading 1</h1>
-                <h2 className="text-xl font-bold">Heading 2</h2>
-                <h3 className="text-lg font-bold">Heading 3</h3>
-                <h4 className="text-base font-bold">Heading 4</h4>
-                <h5 className="text-sm font-bold">Heading 5</h5>
-                <p className="text-base">Paragraph</p>
-                <p className="text-xs">Fine print</p>
-                <small className="text-xxs">Small</small>
+                <h1 className="text-2xl font-bold">Text-2xl - largest</h1>
+                <h2 className="text-xl font-bold">Text-xl - large</h2>
+                <h3 className="text-lg font-bold">Text-lg - medium</h3>
+                <h4 className="text-base font-bold">Text-base - normal</h4>
+                <h5 className="text-sm font-bold">Text-sm - smaller</h5>
+                <p className="text-xs">Text-xs - small</p>
+                <small className="text-xxs">Text-xxs - super small</small>
             </div>
         )
     },
