19566a363629d0a1bff9590dc412ebd23b309034
Thomas Obermüller <thomas.obermueller@gmail.com>
2026-04-01 11:17:30 +0200

    frontend

--- frontend/src/queries/nodes/InsightViz/TrendsSeries.tsx
+++ frontend/src/queries/nodes/InsightViz/TrendsSeries.tsx
@@ -12,7 +12,7 @@ import { keyForInsightLogicProps } from 'scenes/insights/sharedUtils'
 import { getInsightPropertyFilterGroupTypes } from 'scenes/insights/utils/propertyTaxonomicGroupTypes'
 
 import { groupsModel } from '~/models/groupsModel'
-import { FunnelsQuery, LifecycleQuery, NodeKind, StickinessQuery, TrendsQuery } from '~/queries/schema/schema-general'
+import { LifecycleQuery, NodeKind, StickinessQuery, TrendsQuery } from '~/queries/schema/schema-general'
 import { isInsightQueryNode } from '~/queries/utils'
 import { ChartDisplayType, FilterType } from '~/types'
 
@@ -49,7 +49,6 @@ export function TrendsSeries(): JSX.Element | null {
     }
 
     const filters = queryNodeToFilter(querySource)
-    const isFunnels = querySource.kind === NodeKind.FunnelsQuery
     const mathAvailability = isLifecycle
         ? MathAvailability.None
         : isStickiness
@@ -59,6 +58,10 @@ export function TrendsSeries(): JSX.Element | null {
             : display === ChartDisplayType.BoxPlot
               ? MathAvailability.BoxPlotOnly
               : MathAvailability.All
+    const supportsDataWarehouse =
+        (isTrends && display !== ChartDisplayType.CalendarHeatmap && display !== ChartDisplayType.BoxPlot) ||
+        isLifecycle ||
+        isStickiness
 
     return (
         <>
@@ -75,15 +78,6 @@ export function TrendsSeries(): JSX.Element | null {
                                 NodeKind.LifecycleDataWarehouseNode
                             ),
                         } as LifecycleQuery)
-                    } else if (isFunnels) {
-                        updateQuerySource({
-                            series: actionsAndEventsToSeries(
-                                payload as any,
-                                true,
-                                mathAvailability,
-                                NodeKind.FunnelsDataWarehouseNode
-                            ),
-                        } as FunnelsQuery)
                     } else {
                         updateQuerySource({
                             series: actionsAndEventsToSeries(
@@ -112,12 +106,7 @@ export function TrendsSeries(): JSX.Element | null {
                     ...(hasPageview ? [TaxonomicFilterGroupType.PageviewEvents] : []),
                     ...(hasScreen ? [TaxonomicFilterGroupType.ScreenEvents] : []),
                     TaxonomicFilterGroupType.AutocaptureEvents,
-                    ...((isTrends &&
-                        display !== ChartDisplayType.CalendarHeatmap &&
-                        display !== ChartDisplayType.BoxPlot) ||
-                    isLifecycle
-                        ? [TaxonomicFilterGroupType.DataWarehouse]
-                        : []),
+                    ...(supportsDataWarehouse ? [TaxonomicFilterGroupType.DataWarehouse] : []),
                 ]}
                 hideDeleteBtn={series?.length === 1}
                 addFilterDocLink="https://posthog.com/docs/product-analytics/trends/filters"
