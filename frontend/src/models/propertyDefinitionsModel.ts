1989a9f011e09249dc410f109dbbe565847087c4
Frank Hamand <frank@posthog.com>
2026-04-08 15:58:31 +0100

    feat(tracing): implement trace_id/span_id/duration filters

--- frontend/src/models/propertyDefinitionsModel.ts
+++ frontend/src/models/propertyDefinitionsModel.ts
@@ -413,7 +413,7 @@ export const propertyDefinitionsModel = kea<propertyDefinitionsModelType>([
             { endpoint, type, newInput, propertyKey, eventNames, properties, refresh },
             breakpoint
         ) => {
-            if (['cohort', 'log'].includes(type)) {
+            if (['cohort', 'log', 'span'].includes(type)) {
                 return
             }
             if (!propertyKey || values.currentTeamId === null) {
